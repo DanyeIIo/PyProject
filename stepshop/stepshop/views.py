4f2bb3989a61c42bc41f555aa991d00f27030194
itstep <Itstep@new.local.itstep>
2021-08-16 11:56:25 +0600

    second

---
+++ stepshop/stepshop/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contacts(request):
+    return render(request, 'contact.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def products(request):
+    return render(request, 'products.html')
